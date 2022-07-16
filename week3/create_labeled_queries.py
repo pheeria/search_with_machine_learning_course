f9e1136b574ed6bda9cbe8f01090f4a94f1282a4
Olzhas Askar <olzhas.askar@gmail.com>
2022-07-16 11:43:04 +0000

    feat: inspired from Nazareno

--- week3/create_labeled_queries.py
+++ week3/create_labeled_queries.py
@@ -5,6 +5,7 @@ import pandas as pd
 import numpy as np
 import csv
 import re
+from collections import defaultdict
 
 # Useful if you want to perform stemming.
 import nltk
@@ -62,6 +63,25 @@ def transform_name(query: str):
 df['query'] = df['query'].apply(transform_name)
 
 # IMPLEMENT ME: Roll up categories to ancestors to satisfy the minimum number of queries per category.
+parents_dict = dict(parents_df.set_index('category')['parent'])
+category_counts = defaultdict(int)
+category_counts.update(df.category.value_counts())
+
+while True:
+    small = [category for category, count in category_counts.items() if count < min_queries]
+    if not small:
+        break
+    for category in small:
+        category_counts[parents_dict[category]] += category_counts.pop(category)
+
+widener = {}
+for category in categories:
+    widened_category = category
+    while widened_category not in category_counts:
+        widened_category = parents_dict[widened_category]
+    widener[category] = widened_category
+
+df['category'] = df['category'].apply(lambda category: widener[category])
 
 # # Create labels in fastText format.
 df['label'] = '__label__' + df['category']
